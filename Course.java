5c213c789b3c306df2d82b8b476df225b68ffc1d
Shamal47770 <daurenbekenov237@gmail.com>
2026-03-14 21:52:47 +0600

    Update Course.java

--- Course.java
+++ Course.java
@@ -2,17 +2,21 @@ class Course {
 
     private String courseName;
     private String courseCode;
-public Course(String courseName, String courseCode) {
+
+    public Course(String courseName, String courseCode) {
         this.courseName = courseName;
         this.courseCode = courseCode;
     }
-public String getCourseName() {
+
+    public String getCourseName() {
         return courseName;
     }
-public String getCourseCode() {
+
+    public String getCourseCode() {
         return courseCode;
     }
-public void displayCourse() {
+
+    public void displayCourse() {
         System.out.println("Course Name: " + courseName);
         System.out.println("Course Code: " + courseCode);
     }
